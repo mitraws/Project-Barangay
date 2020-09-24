0cbf6547fba1609497df6d40e06ba54bffc32e15
SupriyaShekar123 <s.supriya82@gmail.com>
2020-09-24 14:37:17 +0200

    Rooms component created

---
+++ src/Pages/Rooms/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function index() {
+  return (
+    <div>
+      <h1>THE “TROPICAL” ROOMS</h1>
+      <div>
+        <image />
+      </div>
+      <div>
+        <p>
+          Our non smoking double or twin rooms are decorated in a quirky,
+          tropical, S/E Asian style. Both rooms have large en-suite bathrooms
+          with showers and bathtubs. Unique among B&B's, we have Japanese bidet
+          toilets for complete hygiene. Rooms are on the first and second floor.
+          Like monumental houses in Amsterdam, we have steep narrow stairs, the
+          B&B is not suitable to those physicaly challenged.
+        </p>
+      </div>
+    </div>
+  );
+}
