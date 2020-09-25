d9abc8a668934d466600fc7818827f41ba22c274
SupriyaShekar123 <s.supriya82@gmail.com>
2020-09-25 16:14:09 +0200

    added images and info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 import Rooms from "./Pages/Rooms/index";
 
 function App() {
--- src/Pages/Rooms/index.js
+++ src/Pages/Rooms/index.js
@@ -5,7 +5,38 @@ export default function index() {
     <div>
       <h1>THE “TROPICAL” ROOMS</h1>
       <div>
-        <image />
+        <img
+          src='http://barangay.nl/images/thumbnail%20(4).jpg?crc=390300741'
+          alt='bedroom'
+        />
+        <img
+          src='http://barangay.nl/images/thumbnail%20(1).jpg?crc=238654893'
+          alt='bathroom'
+        />
+
+        <img
+          src='http://barangay.nl/images/thumbnail%20(9).jpg?crc=123493431'
+          alt='bedroom'
+        />
+        <img
+          src='http://barangay.nl/images/thumbnail%20(7).jpg?crc=3937410535'
+          alt='bathroom'
+        />
+        <img
+          src='http://barangay.nl/images/img_6864.jpg?crc=90861977'
+          alt='tabel and desk'
+        />
+        <img
+          src='http://barangay.nl/images/img_6852.jpg?crc=390424378'
+          alt='TV'
+        />
+        <img
+          src='http://barangay.nl/images/img_6859.jpg?crc=60136289'
+          alt='fridge'
+        />
+      </div>
+      <div>
+        <button>Show Availability </button>
       </div>
       <div>
         <p>
@@ -13,9 +44,46 @@ export default function index() {
           tropical, S/E Asian style. Both rooms have large en-suite bathrooms
           with showers and bathtubs. Unique among B&B's, we have Japanese bidet
           toilets for complete hygiene. Rooms are on the first and second floor.
-          Like monumental houses in Amsterdam, we have steep narrow stairs, the
-          B&B is not suitable to those physicaly challenged.
+          Like monumental houses in Amsterdam,{" "}
+          <b>
+            we have steep narrow stairs, the B&B is not suitable to those
+            physicaly challenged
+          </b>
+          .
         </p>
+      </div>
+      <div>
+        <h4>Amenites</h4>
+        <ul>
+          <li>- Free Hi-Speed WIFI</li>
+          <li>
+            {" "}
+            - Modern en-suite Bathroom with bathtub and separate walk-in Power
+            shower and Bath tub
+          </li>
+          <li>- Comfortable Luxe Queen bed or 2 single beds</li>- Small table
+          and 2 chairs - Complimentary quality coffee and tea{" "}
+          <li>- Welcome basket with rose wine and snacks</li>{" "}
+          <li>- No noise / quiet Small fridge </li>
+          <li>- Cable T.V </li>
+          <li>- Alarm clock</li>
+          <li> - Hair Dryer</li>
+          <li> - Silky Bathrobe</li> <li>- Non-gas Heating elements</li>
+          <li>- Aircon</li>
+          <li>- Climate control with air filters - Daily cleaing service </li>
+          <li> -Our "World famous" Breakfast in bed.</li>
+        </ul>
+      </div>
+      <div>
+        <h4>Cancellation Policy</h4>
+        The applied amounts / percentages are to be paid by the guest. If you
+        cancel you reservation Less than 7 days before date of arrival & NO
+        SHOW: 100% of total amount. More than 7 days before arrival date: 25
+        euro administration fee. Always read your confirmation email !! We
+        strongly recommend to get a personal travel insurance that will provide
+        adequate cover during your stay and will also compensate you if you need
+        to cancel or cannot meet your obligations regarding your booked
+        accommodations.
       </div>
     </div>
   );
