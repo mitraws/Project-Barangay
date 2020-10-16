002ccb809fe8dd4ca82502c7ebc505d7980cbd4b
Mitraws <m.ws@outlook.com>
2020-10-16 16:02:51 +0200

    Refactor styling

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -4,27 +4,24 @@ import "./index.css";
 export default function index() {
   return (
     <div className="outer_footer">
-      <div className='footer_color'>
+      <div className="footer_color">
         <footer>
-          <div className='footerGrid'>
+          <div className="footerGrid">
             <div>
-              <p>Barangay B&B </p>
-              <p>Wimmo & Godwin</p> <p>Herenstraat 26</p>{" "}
-              <p>1015CB, Amsterdam </p>
-              <p>The Netherlands</p>
+              <h5>Barangay B&B </h5>
+              <h5>Wimmo & Godwin</h5>
+              <h5>Herenstraat 26</h5> <h5>1015CB, Amsterdam </h5>
+              <h5>The Netherlands</h5>
             </div>
             <div>
-              <p>Rooms</p>
-
-              <p>Good To Know</p>
-
-              <p>About Us</p>
-
-              <p>Contact</p>
+              <h5>Rooms</h5>
+              <h5>Good To Know</h5>
+              <h5>About Us</h5>
+              <h5>Contact</h5>
             </div>
             <div>
-              <p>Email: rooms@barangay.nl</p>
-              <p>Telephone: +31 (0) 62504 5432</p>
+              <h5>Email: rooms@barangay.nl</h5>
+              <h5>Telephone: +31 (0) 62504 5432</h5>
             </div>
           </div>
         </footer>
