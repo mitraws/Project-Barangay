399ce34221249366d7e030a9137bdbec337b2957
SupriyaShekar123 <s.supriya82@gmail.com>
2020-10-16 09:57:06 +0200

    added extenal booking link

--- src/Components/Button/index.js
+++ src/Components/Button/index.js
@@ -1,10 +1,23 @@
 import React from "react";
+
+
 import "./index.css";
 
+
 export default function index(props) {
+  function external(e){
+    
+    e.preventDefault();
+      window.location.href='https://reservations.cubilis.eu/bbbarangay-amsterdam?Language=en-GB';
+  }
+  
   return (
     <div>
-      <button className="contact-right">Book Now</button>
+      
+
+
+      <button className="contact-right" onClick={external}>{props.name}</button>
+       
     </div>
   );
 }
